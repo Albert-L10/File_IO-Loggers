a8ffe0e73053664ab4350c74e8cfee490ad7e141
Albert-L10 <albert.legodi@umuzi.org>
2020-07-17 12:46:27 +0200

    Updated project

--- src/main/java/Visitor.java
+++ src/main/java/Visitor.java
@@ -1,72 +1,128 @@
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
-import java.util.logging.Logger;
+
+
 
 public class Visitor {
 
-    private static final Logger logger = (Logger) LogManager.getLogger(Visitor.class.getName());
 
-    static String fullName;
-    static int age;
-    static String dateOfVisit;
-    static String timeOfVisit;
-    static String comments;
-    static String nameOfContact;
+    private static final Logger logger = LogManager.getLogger(Visitor.class.getName());
+
+
+    public static boolean saveFile(String fullName, int age, String comments, String nameOfContact)throws IOException {
+
+        FileWriter fileWriter = null;
+
+        new File("visitor_ " + fullName.replace(" ", "_") + ".txt");
+
+        File visitorFile;
+
+        try {
+            if (!fullName.isEmpty()) {
+                String full_Name = fullName.replace(" ", "_");
+                visitorFile = new File("visitor_" + full_Name.toLowerCase() + ".txt");
+
+
+                if (visitorFile.createNewFile()) {
+                    fileWriter = new FileWriter(visitorFile);
+                    fileWriter.write(fullName + " \n" +
+                            age + " \n" +
+                            "Date of visit: " + LocalDate.now() + " \n" +
+                            "timeOfVisit:" + LocalTime.now() + " \n" +
+                            comments + " \n" +
+                            nameOfContact);
+
+                    logger.info("file created successfully");
+                } else {
+                    logger.debug("file already exist!");
+                }
+            }
+            else{
+                logger.error("File is not created");
+            }
+
+
+
+        }
+        catch (IOException e)
+        {
+            logger.info(saveFile(fullName,age,comments, nameOfContact));
+            logger.error(saveFile(fullName,age,comments, nameOfContact));
+            e.printStackTrace();
+        }
+        assert fileWriter != null;
+        fileWriter.close();
+
+        return true;
+    }
+
+    public static void loadFile(String name) {
+
+
+            File myObj = new File("visitor_" +name.toLowerCase().replace(" ", "_") + ".txt");
+
+            if(!myObj.exists()) {
+               logger.error("File not found");
+            }
+            else {
+                System.out.println("File name: " + myObj.getName());
+            }
+
+    }
+
 
 
     public static void main(String[] args) throws IOException {
 
-        String name="Albert legodi";
-        fullName=name.replace(' ','_');
-        age=20;
-        dateOfVisit="15/03/2020" ;
-        timeOfVisit="11:05";
-        comments="Umuzi is cold!";
-        nameOfContact="Paul";
-        Visitor Albert=new Visitor();
+    Scanner input=new Scanner(System.in);
 
-        Albert.save(fullName,age,dateOfVisit,timeOfVisit,comments,nameOfContact);
 
-       Albert.load(fullName);
+        System.out.println("enter 1 to Load OR  2 to Save");
+       int option=input.nextInt();
 
-    }
-    public String save(String fullName, int age, String dateOfVisit, String timeOfVisit, String comments, String nameOfContact) throws IOException {
 
-        try {
-            String full_Name = fullName.replace(" ", "_");
-            FileWriter fileWriter = new FileWriter("visitor_" + full_Name.toLowerCase() + ".txt");
-            fileWriter.write(fullName + " \n" + age + " \n" + dateOfVisit + " \n" + timeOfVisit + " \n" + comments + " \n" + nameOfContact);
-            fileWriter.close();
+        switch(option) {
 
-            logger.info("file created");
-        }
-        catch (IOException e)
-        {
-            logger.info(e.getMessage());
-        }
-        return "File is created!";
-    }
+            case 1:
 
-    public static void load(String name) {
-        try {
+                System.out.println("Enter your name");
+                String name = input.next();
+                loadFile(name);
 
-            File myObj = new File("Visitor_" +name.toLowerCase().replace(" ", "_") + ".txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String Albert = myReader.nextLine();
+                break;
 
-            myReader.close();
-        }
-    } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            logger.warning("An error occurred.");
+            case 2:
+                System.out.println("Enter your name");
+                 name = input.next();
+
+                System.out.println("Enter your age");
+                int age = input.nextInt();
+
+                System.out.println("Enter name of someone who assisted you?");
+                String contactPerson = input.next();
+
+
+                System.out.println("Any comments?");
+                String comments = input.next();
+                saveFile(name, age, comments, contactPerson);
+                break;
+
+
+
+            default:
+                System.out.println("invalid option");
+
         }
     }
+
+
 
 }
 
